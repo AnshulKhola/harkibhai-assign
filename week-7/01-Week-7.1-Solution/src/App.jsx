13a212d7c68d76eaaecd03de656e1f842d04d998
AnshulKhola <golukhola18@gmail.com>
2024-06-23 15:39:58 +0530

    Now Completed Week-7.1

--- week-7/01-Week-7.1-Solution/src/App.jsx
+++ week-7/01-Week-7.1-Solution/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Suspense, useState } from 'react'
+import { lazy } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
+// import Landing from './components/Landing'
+// import Dashboard from './components/Dashboard'
+const Landing  = lazy(()=> import ('./components/Landing'));
+const Dashboard  = lazy(()=> import ('./components/Dashboard'));
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <BrowserRouter>
+      <Appbar/>
+      <Routes>
+        {/* Now Suspense Api is usefull as there is some time when we load some data in the pages which make them slow so we add suspense api as when new page is loading it shows loading... on the sreen */}
+        <Route path="/" element={<Suspense fallback={"loading..."}><Landing/></Suspense>} />
+        <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard/></Suspense>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => navigate('/')}>Landing</button>
+      <button onClick={() => navigate('/dashboard')}>Dashboard</button>
     </>
   )
 }
